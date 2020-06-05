2b90524716dcf69a977d6f8559638635834a9d32
Larocca <roberto.larocca@nttdata.com>
2020-06-05 16:31:54 +0200

    fix

--- my-blog/src/app/blog-form/blog-form.component.ts
+++ my-blog/src/app/blog-form/blog-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import {Blog} from '../model/Blog'
+import { SiteClient } from 'datocms-client';
 
 @Component({
   selector: 'app-blog-form',
@@ -8,6 +9,7 @@ import {Blog} from '../model/Blog'
   styleUrls: ['./blog-form.component.less']
 })
 export class BlogFormComponent implements OnInit {
+  client: SiteClient = new SiteClient('b26afeae0277286f8059752d5b4b5c'); 
   model: Blog = new Blog();
 
   constructor(private route: ActivatedRoute,
@@ -19,12 +21,17 @@ export class BlogFormComponent implements OnInit {
   onSubmit() { 
     const articoloNuovo = {
       itemType: '238961', // model ID
-      titoloArticolo: this.model.titoloArticolo,
+      titoloArticolo: {en: this.model.titoloArticolo, it: this.model.titoloArticolo}, 
       mostraCopertina: false,
       immagine_copertina: null,
-      body: this.model.body,
+      body: {en: this.model.body, it: this.model.body}, 
+      luogo: null,
+      attachment: null,
+      articoli_correlati: null,
     };
-
+    console.log("articoloNuovo: " + articoloNuovo);
+    this.client.items.create(articoloNuovo);
+    alert("Articolo creato correttamente.");
   }
 
   get diagnostic() { return JSON.stringify(this.model); }
